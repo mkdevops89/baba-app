fb2f5ba10c7f41b566b0fbf9b7b525b43c551885
mkdevops89 <mlis.dev89@gmail.com>
2026-09-21 09:40:12 -0500

    fix: allow CloudTrail replication refresh (#47)
    Co-authored-by: blisboa <mlis682@gmail.com>

--- 03-terraform/modules/automation-iam/main.tf
+++ 03-terraform/modules/automation-iam/main.tf
@@ -461,6 +461,7 @@ data "aws_iam_policy_document" "phase09_security_automation" {
       "s3:ListBucket",
       "s3:GetAccelerateConfiguration",
       "s3:GetLifecycleConfiguration",
+      "s3:GetReplicationConfiguration",
       "s3:GetBucket*",
       "s3:PutBucket*",
       "s3:DeleteBucket*"
